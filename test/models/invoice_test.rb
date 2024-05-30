edd164647a6b228d612ac83031cbf77ab04b775e
Yaroslav Shmarov <yshmarov@gmail.com>
2024-05-30 20:03:59 +0200

    test "xml_document_to_json"

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class InvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "xml_document_to_json" do
+    invoice = Invoice.create
+    file_path = 'db/fixtures/factur-x/BASIC/BASIC_Einfach.pdf'
+    invoice.pdf_document.attach(io: File.open(file_path), filename: 'BASIC_Einfach.pdf')
+    PdfBlobToXmlJob.perform_now(invoice)
+
+    assert_match "<?xml version=", invoice.xml_document
+    due_payable_amount = invoice.xml_document_to_json.dig('rsm:CrossIndustryInvoice', 'rsm:SupplyChainTradeTransaction', 'ram:ApplicableHeaderTradeSettlement', 'ram:SpecifiedTradeSettlementHeaderMonetarySummation', 'ram:DuePayableAmount')
+    assert_match "235.62", due_payable_amount
+  end
 end
